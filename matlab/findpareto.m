ef8c480740fd0bf8ababc8eacffcc6e7025e68da
Edward L Platt <ed@elplatt.com>
2014-01-19 15:36:24 -0500

    Reformat for ICWSM specs.

--- matlab/findpareto.m
+++ matlab/findpareto.m
@@ -8,7 +8,7 @@
 % http://esapubs.org/Archive/ecol/E089/052/mle_discretepareto.m
 %
 
-exp_id = '1390097300.74'
+exp_id = '1390159603.95'
 
 % Load data
 filename = strcat('../results/findstatistics/', exp_id, '/spread_span.csv');
@@ -25,12 +25,24 @@ hold on;
 % Fit and plot pareto
 xx = linspace(1, max(spread), 10000);
 shape = mle_discretepareto(spread);
+strcat('Global Reach Pareto shape: ', num2str(shape))
 loglog(xx, xx.^shape ./ zeta(-shape));
-xlabel('Global Reach (nations)');
-ylabel('Frequency');
-title('Global Reach of Trending Videos');
-legend('Observed', strcat('Best-fit Pareto (\lambda = ', num2str(shape), ')'));
+hx = xlabel('Global Reach (nations)');
+hy = ylabel('Frequency');
+ht = title('Global Reach of Trending Videos');
+hl = legend('Observed', strcat('ML Pareto'));
 grid on;
+set(h, 'Units', 'inches')
+set(h, 'OuterPosition', [0 0 9.375 7.03125])
+set(h, 'PaperPosition', [0 0 3.125 2.34375])
+set(hl, 'FontSize', 7)
+set(ht, 'FontSize', 10)
+set(hx, 'FontSize', 10)
+set(hy, 'FontSize', 10)
+set(gca, 'FontSize', 6)
+set(gca, 'XTick', [1 10])
+set(gca, 'YTick', [0.00001 0.0001 0.001 0.01 0.1 1])
+axis([1 60 0.00001 1])
 
 % Save figure
 saveas(h, strcat('../results/findstatistics/', exp_id, '/spread.fig'));
@@ -45,12 +57,24 @@ hold on;
 % Fit and plot pareto
 xx = linspace(1, max(span), 10000);
 shape = mle_discretepareto(span);
+strcat('Lifespan Pareto shape: ', num2str(shape))
 loglog(xx, xx.^shape ./ zeta(-shape));
-xlabel('Lifespan (days)');
-ylabel('Frequency');
-title('Lifespan of Trending Videos');
-legend('Observed', strcat('Best-fit Pareto (\lambda = ', num2str(shape), ')'));
+hx = xlabel('Lifespan (days)');
+hy = ylabel('Frequency');
+ht = title('Lifespan of Trending Videos');
+hl = legend('Observed', strcat('ML Pareto'));
 grid on;
+set(h, 'Units', 'inches')
+set(h, 'OuterPosition', [0 0 9.375 7.03125])
+set(h, 'PaperPosition', [0 0 3.125 2.34375])
+set(hl, 'FontSize', 7)
+set(ht, 'FontSize', 10)
+set(hx, 'FontSize', 10)
+set(hy, 'FontSize', 10)
+set(gca, 'FontSize', 6)
+set(gca, 'XTick', [1 10 100])
+set(gca, 'YTick', [0.00001 0.0001 0.001 0.01 0.1 1])
+axis([1 200 0.00001 1])
 
 % Save figure
 saveas(h, strcat('../results/findstatistics/', exp_id, '/span.fig'));
