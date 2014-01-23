4f8357f4679008224f588094b8ce16ab66dab8d7
Edward L Platt <ed@elplatt.com>
2014-01-22 20:27:39 -0500

    Change test stubs from ln to log2 for exposure distance.

--- tests/stubs.py
+++ tests/stubs.py
@@ -39,12 +39,12 @@ dir_ex.add_edge(4, 3, weight=1/6)
 
 # Symmetric exposure graph for test data
 sym_ex = nx.Graph()
-sym_ex.add_edge(0, 1, weight=1/3, distance=1.09861228867)
-sym_ex.add_edge(0, 2, weight=1/3, distance=1.09861228867)
-sym_ex.add_edge(1, 3, weight=1/3, distance=1.09861228867)
-sym_ex.add_edge(2, 3, weight=1/3, distance=1.09861228867)
-sym_ex.add_edge(3, 4, weight=1/3, distance=1.09861228867)
-sym_ex.add_edge(1, 4, weight=5/12, distance=0.87546873735)
+sym_ex.add_edge(0, 1, weight=1/3, distance=1.58496250072)
+sym_ex.add_edge(0, 2, weight=1/3, distance=1.58496250072)
+sym_ex.add_edge(1, 3, weight=1/3, distance=1.58496250072)
+sym_ex.add_edge(2, 3, weight=1/3, distance=1.58496250072)
+sym_ex.add_edge(3, 4, weight=1/3, distance=1.58496250072)
+sym_ex.add_edge(1, 4, weight=5/12, distance=1.26303440583)
 
 # Exposure graph degrees
 in_degree = [1, 7/6, 2/3, 1/2, 5/6]
